ce44264af3c71186e2bb9a43af94b4e3b101fce9
jdidi94 <jdididaoud404@gmail.com>
2022-02-22 19:18:33 +0100

    variables exercices

---
+++ index.js
@@ -0,0 +1,10 @@
+//  1.What are the types of these variables listed below? Verify this with typeof and
+// output the result to the console:
+let str1 = "Laurence";
+let str2 = "Svekis";
+let val1 = undefined;
+let val2 = null;
+let myNum = 1000;
+// use type of build in function to know the type
+// 2.declare variable with let const and var
+// 3.declare the pi =3.14 constante , other three variables calls them miles,kilometres,meters
